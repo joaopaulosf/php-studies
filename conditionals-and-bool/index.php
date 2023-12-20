95941bce22df2d09d2e867a08460629ae8143f7a
joaopaulosf <jpforcode@gmail.com>
2023-12-20 13:00:50 -0300

    feat: implement conditionals and bool

---
+++ conditionals-and-bool/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+</head>
+
+<body>
+
+    <?php
+        $name = 'Dark Matter';
+        $read = true;
+
+        if($read) {
+            $message = 'You have read ' .  $name;
+        } else {
+            $message = 'You have NOT read ' .  $name;
+        }
+    ?>
+    <h1>
+        <!--    echo shortcut-->
+        <?= $message; ?>'.
+    </h1>
+
+</body>
+
+</html>
