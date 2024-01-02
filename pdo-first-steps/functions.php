021c4a5fabd5daaf969e34983bbbd41a8f79a5d7
joaopaulosf <jpforcode@gmail.com>
2024-01-02 20:11:39 -0300

    feat: implement pdo-first-steps

---
+++ pdo-first-steps/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+function dd($value) // 'display dump'
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+    die();
+}
+
+function urlIs(string $value): bool
+{
+    return $_SERVER['REQUEST_URI'] === $value;
+}
---
+++ pdo-first-steps/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'functions.php';
+require_once realpath(__DIR__ . "/vendor/autoload.php");
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$dsn = 'mysql:host=localhost;port:3306;dbname=myapp;charset=utf8mb4';
+$username = $_ENV['DB_USERNAME'];
+$pwd = $_ENV['DB_PASSWORD'];
+
+// dsn = data source name.
+$pdo = new PDO($dsn, $username, $pwd);
+$statement = $pdo->prepare("SELECT * FROM myapp.POSTS");
+$statement->execute();
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . " | " . $post['created_at'] . "</li>";
+}
