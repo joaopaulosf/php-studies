17df09da3dc630d42014f08516bfb2a33b5e8c12
joaopaulosf <jpforcode@gmail.com>
2024-01-15 20:37:20 -0300

    feat: intro to form validations

--- forms/controllers/note-create.php
+++ forms/controllers/note-create.php
@@ -7,15 +7,30 @@ $db = new Database($config);
 
 $currentUserId = 3;
 
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query("INSERT INTO NOTES (title, body, user_id) VALUES (:title, :body, :user_id)",
-        [
-            'title' => $_POST["title"],
-            'body' => $_POST["body"],
-            'user_id' => $currentUserId
-        ]
-    );
+    $errors = [];
+
+    if (strlen($_POST['title']) === 0) {
+        $errors['title'] = 'A title is required';
+    }
+
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'The body can not be more than 1,000 characters.';
+    }
+
+    if (empty($errors)) {
+        $db->query("INSERT INTO NOTES (title, body, user_id) VALUES (:title, :body, :user_id)",
+            [
+                'title' => htmlspecialchars($_POST["title"]),
+                'body' => htmlspecialchars($_POST["body"]),
+                'user_id' => $currentUserId
+            ]
+        );
+    }
 }
 
 require "views/note-create.view.php";
--- forms/views/note-create.view.php
+++ forms/views/note-create.view.php
@@ -20,27 +20,37 @@ require 'partials/head.php' ?>
                                        class="block text-sm font-medium leading-6 text-gray-900">Title</label>
                                 <div class="mt-2">
                                     <input type="text" placeholder="Text here..." id="title" name="title"
-                                           class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+                                           class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1
+                                           ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2
+                                           focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                 </div>
-                                <!--                                <p class="mt-3 text-sm leading-6 text-red-600">Title is required</p>-->
+                                <?php if ($errors['title']): ?>
+                                    <p class="mt-3 text-sm leading-6 text-red-600"><?= $errors['title'] ?></p>
+                                <?php endif; ?>
                             </div>
 
                             <div class="col-span-full">
                                 <label for="body"
                                        class="block text-sm font-medium leading-6 text-gray-900">Description</label>
                                 <div class="mt-2">
-                                <textarea placeholder="Text here..." id="body" name="body" rows="3"
-                                          class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                                    <textarea placeholder="Text here..." id="body" name="body" rows="3"
+                                              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1
+                                    ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset
+                                    focus:ring-indigo-600 sm:text-sm
+                                    sm:leading-6"><?= $_POST['body'] ?? '' ?></textarea>
                                 </div>
-                                <!--                                <p class="mt-3 text-sm leading-6 text-red-600">Description is required</p>-->
+                                <?php if ($errors['body']) : ?>
+                                    <p class="mt-3 text-sm leading-6 text-red-600"><?= $errors['body'] ?></p>
+                                <?php endif; ?>
                             </div>
                         </div>
                     </div>
 
                     <div class="mt-6 flex items-center justify-end gap-x-6">
 
-                        <button type="submit"
-                                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm
+                        hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
+                        focus-visible:outline-indigo-600">
                             Save
                         </button>
                     </div>
